5e9d0bc4ab40982b4571e7206ca374ce5590e4da
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-11-29 11:13:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$(document).ready (function (){
-    $('.btn').on('click',function(){
-        $("#result-disply").val()($("#result-disply").val() +(this).val())
+$(document).ready (function (e){
+    $('.btn').click(function(e) {
+      $("#result-disply").val($("#result-disply").val() + $(this).val())
+   
     });
-})
 
+    $('#Remove').click(function(e) {
+        $('#result-disply').val('') ;
+     
+    });
+    $('#equal').click(function(e){
+        $('#result-disply').val(eval($('#result-disply').val()));
+    });
 
+});
